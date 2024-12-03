1684cdca4fecb72c853dd7deaabc733fa2e70e23
rajveeerr <rajveergreets@gmail.com>
2024-12-04 03:43:04 +0530

    fixed the bugs where draggable was set to true

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CustomizationPage from './pages/customizationPaje';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Layout from './pages/layout';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 
@@ -31,6 +32,7 @@ function App() {
       {/* <ChatBot/> */}
     </RecoilRoot>
     <Analytics/>
+    <SpeedAnalytics/>
     </>)
 }
 
